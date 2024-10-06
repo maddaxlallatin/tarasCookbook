f81afeeb23a1d3cb403a6ab8c21c43b502142063
Maddax Lallatin <65627323+maddaxlallatin@users.noreply.github.com>
2024-10-06 15:31:37 -0600

    updated url

--- frontEnd/src/axiosConfig.js
+++ frontEnd/src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default axios.create({
 
-    baseURL: "http://www.tarascookbook.live:8080/cookbook-backend-0.0.1-SNAPSHOT/api/v1",
+    baseURL: "http://api.tarascookbook.live/cookbook/api/v1",
     headers: {
         " Access-Control-Allow-Origin": "*",
         "Content-type": "application/json"
